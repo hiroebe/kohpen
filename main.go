54a3c47cdbe0885822e367adf2e56efff93ae48a
hiroebe <hiro.ebe.albirex12@gmail.com>
2020-03-12 18:23:39 +0900

    use os.Getenv("PORT") instead of flag

--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"net/http"
+	"os"
 )
 
-var (
-	port = flag.Int("port", 8080, "port")
-)
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("$PORT is not set")
+	}
+	return port
+}
 
 func main() {
-	flag.Parse()
+	port := getPort()
 
 	http.Handle("/", http.FileServer(http.Dir("./web/public")))
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(w, r)
 	})
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
 	}
 }
